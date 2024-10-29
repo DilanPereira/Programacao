f050a4539d831d17986919a73c95a8827a40951d
DilanPereira <dilanmpereira2002@gmail.com>
2024-10-29 10:17:33 +0000

    Ficha2-2 concluida correcao

--- IntroducaoAProgramacao/src/Ficha22/Ex18.java
+++ IntroducaoAProgramacao/src/Ficha22/Ex18.java
@@ -6,17 +6,17 @@ public class Ex18 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Insira o preço original:");
-        float precoOriginal = teclado.nextFloat();
+        double precoOriginal = teclado.nextDouble();
         System.out.println("Insira o desconto em percentagem:");
         int descontoPerc = teclado.nextInt();
 
         double precoFinal = precoOriginal * (1 - descontoPerc / 100.0);
         double desconto = precoOriginal - precoFinal;
 
-        System.out.printf("Preço: %,5.2f\n", precoOriginal);
-        System.out.printf("Desconto em percentagem: %,3d\n", descontoPerc);
-        System.out.printf("Preço final: %,5.2f\n", precoFinal);
-        System.out.printf("Desconto: %.2f\n", desconto);
+        System.out.printf("%-30s | %10.2f\n","Preço:", precoOriginal);
+        System.out.printf("%-30s | %10d\n","Desconto em percentagem:", descontoPerc, " ");
+        System.out.printf("%-30s | %10.2f\n","Preço final:", precoFinal);
+        System.out.printf("%-30s | %10.2f\n","Desconto:", desconto);
 
 
         teclado.close();
